b496fbdad5666e27884a7798f8737c47af78c287
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2022-09-05 18:25:15 +0200

    Switch to Catch2

--- test/computeBallCenter.cpp
+++ test/computeBallCenter.cpp
@@ -1,45 +1,45 @@
 #include "../src/bpa.cpp"
 
+#include <catch2/catch_test_macros.hpp>
 #include <glm/gtx/io.hpp>
-#include <gtest/gtest.h>
 
 using namespace bpa;
 
-TEST(computeBallCenter, isosceles) {
+TEST_CASE("isosceles", "[computeBallCenter]") {
 	MeshPoint a{vec3{0, 0, 0}};
 	MeshPoint b{vec3{10, 0, 0}};
 	MeshPoint c{vec3{0, 10, 0}};
 	MeshFace f{&a, &b, &c};
 	const auto center = computeBallCenter(f, 10);
-	EXPECT_TRUE(center);
-	EXPECT_EQ(center.value(), (vec3{5, 5, 7.07106781f}));
+	REQUIRE(center);
+	CHECK(center.value() == (vec3{5, 5, 7.07106781f}));
 }
 
-TEST(computeBallCenter, isoscelesLargerRadius) {
+TEST_CASE("isoscelesLargerRadius", "[computeBallCenter]") {
 	MeshPoint a{vec3{0, 0, 0}};
 	MeshPoint b{vec3{10, 0, 0}};
 	MeshPoint c{vec3{0, 10, 0}};
 	MeshFace f{&a, &b, &c};
 	const auto center = computeBallCenter(f, 100);
-	EXPECT_TRUE(center);
-	EXPECT_EQ(center.value(), (vec3{5, 5, 99.7496872f}));
+	REQUIRE(center);
+	CHECK(center.value() == (vec3{5, 5, 99.7496872f}));
 }
 
-TEST(computeBallCenter, equilateral) {
+TEST_CASE("equilateral", "[computeBallCenter]") {
 	MeshPoint a{vec3{0, 0, 0}};
 	MeshPoint b{vec3{10, 0, 0}};
 	MeshPoint c{vec3{5, std::sqrt(3.0f) * 10.0f / 2.0f, 0}};
 	MeshFace f{&a, &b, &c};
 	const auto center = computeBallCenter(f, 10);
-	EXPECT_TRUE(center);
-	EXPECT_EQ(center.value(), (vec3{4.99999952f, 2.88675070f, 8.16496658f}));
+	REQUIRE(center);
+	CHECK(center.value() == (vec3{4.99999952f, 2.88675070f, 8.16496658f}));
 }
 
-TEST(computeBallCenter, radiusTooSmall) {
+TEST_CASE("radiusTooSmall", "[computeBallCenter]") {
 	MeshPoint a{vec3{0, 0, 0}};
 	MeshPoint b{vec3{10, 0, 0}};
 	MeshPoint c{vec3{0, 10, 0}};
 	MeshFace f{&a, &b, &c};
 	const auto center = computeBallCenter(f, 1);
-	EXPECT_FALSE(center);
+	CHECK(!center);
 }
